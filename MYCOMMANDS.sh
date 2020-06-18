eb29a90c7dee0ea07988af96b0d7cc57ecd4d894
steve <steve@stery>
2020-06-18 22:32:29 +0200

    update

--- MYCOMMANDS.sh
+++ MYCOMMANDS.sh
@@ -9,6 +9,7 @@ make 40percentclub/gherkin:stevexyz
 bin/qmk flash -kb 40percentclub/gherkin -km stevexyz
 (initiate programming short-circuiting two consecutive times the pins as displayed here: https://learn.sparkfun.com/tutorials/pro-micro--fio-v3-hookup-guide/troubleshooting-and-faq)
 
+cd keyboards/bm43a/keymaps/stevexyz/
 make bm43a:stevexyz
 cd ../../../.. ; make bm43a:stevexyz ; cd -
 bin/qmk flash -kb bm43a -km stevexyz
