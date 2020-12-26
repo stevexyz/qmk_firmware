8d403af3731f21c5abc39cf51ec83670c35d5a13
steve <steve@ultra.name>
2020-12-26 23:36:39 +0100

    manjaro

--- MYCOMMANDS.sh
+++ MYCOMMANDS.sh
@@ -1,5 +1,3 @@
-git pull https://github.com/qmk/qmk_firmware.git master
-
 make git-submodule
 
 cd keyboards/jj40/keymaps/stevexyz/
@@ -11,6 +9,9 @@ make 40percentclub/gherkin:stevexyz
 bin/qmk flash -kb 40percentclub/gherkin -km stevexyz
 (initiate programming short-circuiting two consecutive times the pins as displayed here: https://learn.sparkfun.com/tutorials/pro-micro--fio-v3-hookup-guide/troubleshooting-and-faq)
 (figure promicro-programming.png in this directory)
+issues:
+on ubunto remove modem manager package
+on manjaro use sudo (and maybe also remove modem manager from software) - note a single reset is enough
 
 cd keyboards/bm43a/keymaps/stevexyz/
 make bm43a:stevexyz
